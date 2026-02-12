266d96e2ec67bb1a66c5eb917fc8fb66d4be9b11
maimoon2546 <maimoon2546@gmail.com>
2026-02-12 14:25:50 +0700

    fix cors middleware

--- BACK_END/middleware.js
+++ BACK_END/middleware.js
@@ -1,18 +1,17 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const origin = request.headers.get("origin");
 
   const response = NextResponse.next();
 
   response.headers.set(
     "Access-Control-Allow-Origin",
-    origin || "*"
+    "*"
   );
 
   response.headers.set(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
+    "GET,POST,PUT,DELETE,OPTIONS"
   );
 
   response.headers.set(
