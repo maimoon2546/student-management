7c595e6e3a33603f7653d16995c59e6e9823650e
maimoon2546 <maimoon2546@gmail.com>
2026-02-12 14:11:32 +0700

    fix cors allow vercel

--- BACK_END/middleware.js
+++ BACK_END/middleware.js
@@ -1,18 +1,12 @@
-import { NextResponse } from 'next/server';
-
 export function middleware(request) {
   const response = NextResponse.next();
 
-  const allowedOrigins = [
-    "http://localhost:3000",
-    "https://student-management-zco5.vercel.app"
-  ];
-
   const origin = request.headers.get("origin");
 
-  if (allowedOrigins.includes(origin)) {
-    response.headers.set("Access-Control-Allow-Origin", origin);
-  }
+  response.headers.set(
+    "Access-Control-Allow-Origin",
+    origin || "*"
+  );
 
   response.headers.set(
     "Access-Control-Allow-Methods",
@@ -26,7 +20,3 @@ export function middleware(request) {
 
   return response;
 }
-
-export const config = {
-  matcher: "/api/:path*",
-};
