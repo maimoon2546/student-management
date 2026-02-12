aee3ad6ee8bd8198db4a81cbd0af2506ac184758
maimoon2546 <maimoon2546@gmail.com>
2026-02-12 13:50:28 +0700

    fix middleware

--- BACK_END/middleware.js
+++ BACK_END/middleware.js
@@ -1,25 +1,32 @@
-// middleware.js
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
   const response = NextResponse.next();
 
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://student-management-zco5.vercel.app"
+  ];
+
+  const origin = request.headers.get("origin");
+
+  if (allowedOrigins.includes(origin)) {
+    response.headers.set("Access-Control-Allow-Origin", origin);
+  }
+
   response.headers.set(
-    'Access-Control-Allow-Origin',
-    'http://localhost:3000'
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,OPTIONS"
   );
+
   response.headers.set(
-    'Access-Control-Allow-Methods',
-    'GET,POST,PUT,DELETE,OPTIONS'
-  );
-  response.headers.set(
-    'Access-Control-Allow-Headers',
-    'Content-Type, Authorization'
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
   );
 
   return response;
 }
 
 export const config = {
-  matcher: '/api/:path*',
+  matcher: "/api/:path*",
 };
